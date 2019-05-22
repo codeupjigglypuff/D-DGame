678e7ef3c69831dca27d3c8edb5c91fa148bc4f8
arleneamericamartinez <hubacek1221>
2019-05-22 14:38:46 -0500

    lol

--- src/Enemy/Kobold.java
+++ src/Enemy/Kobold.java
@@ -4,29 +4,19 @@ import weapons.Weapon;
 
 public class Kobold extends Enemy {
 
-    private String type = UniversalStats.ETP(Species);
-    private Weapon[] possibleWeapons = UniversalStats.CBW("Kobold",this.type);
-    private Weapon weapon = this.possibleWeapons[(int)(Math.round(Math.random()*(this.possibleWeapons.length)))];
-    private int hth = UniversalStats.CBH(this.Species,this.type);
-    private int dmg = UniversalStats.WBD(this.weapon);
-    private int str = 7 + (int)(Math.round(Math.random()*3));
-    private int dex = 15 + (int)(Math.round(Math.random()*5));
-    private int wis = 7 + (int)(Math.round(Math.random()*2));
+    public static String KoboldType = UniversalStats.ETP("Kobold");
+    public static Weapon[] possibleWeapons = UniversalStats.CBW("Kobold",this.type);
+    public static Weapon KoboldsWeapon = this.possibleWeapons[(int)(Math.round(Math.random()*(this.possibleWeapons.length)))];
+    public static int KoboldsHealth = UniversalStats.CBH("Kobold",KoboldType);
+    public static int KoboldsDamage = KoboldsWeapon.getDmg() + (Math.round(str-10)/2);
+    public static int KoboldsStrength = 7 + (int)(Math.round(Math.random()*3));
+    public static int KoboldsDexterity = 15 + (int)(Math.round(Math.random()*5));
+    public static int KoboldsWisdom = 7 + (int)(Math.round(Math.random()*2));
 
 
 
     public Kobold(){
-        String KoboldsType = type;
-        Weapon KoboldsWeapon = weapon;
-        int KoboldsHealth = hth;
-        int KoboldsDamage = KoboldsWeapon.getDmg() + (str-10)/2;
-        int KoboldsStrength = str;
-        int KoboldsDexterity = dex;
-        int KoboldsWisdom = wis;
-        super("Kobold",KoboldsType,KoboldsWeapon,KoboldsHealth,KoboldsDamage,KoboldsStrength,KoboldsDexterity,KoboldsWisdom);
-
-
-
+        super("Kobold",UniversalStats.ETP("Kobold"),KoboldType,KoboldsHealth,KoboldsDamage,KoboldsStrength,KoboldsDexterity,KoboldsWisdom);
 
     }
 
