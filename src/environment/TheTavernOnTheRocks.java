b430d006852a209a1101a30aae90d447f56cf7b0
John De Leon <johndeleon@MacBook-Pro-3.local>
2019-05-27 20:00:04 -0500

    working on taver

--- src/environment/TheTavernOnTheRocks.java
+++ src/environment/TheTavernOnTheRocks.java
@@ -3,20 +3,23 @@ package environment;
 public class TheTavernOnTheRocks extends Dungeon{
 
     private boolean firstTime = false;
+    private String firstTimeDiscription = (
+            "You find yourself in a tavern like many others. Lively, smokey, reeking of ale " +
+                    "and men. A foul stench that sticks in your nose long after you have " +
+                    "left. You sip on your ale and wonder if another drunken evening awaits you. \n" +
+                    "The Tavern is made of a motley crew of bottom dwellers. A company of " +
+                    "whom" +
+                    " you’ve grown accustom to. Years of bad luck and failed adventures have " +
+                    "lead you to this point and you are not sure if it is a good thing or not.  \n"
+    );
+
+    private String description = (
+            ""
+            );
 
 
     public TheTavernOnTheRocks() {
         super(false, false, true, false);
-
-        this.setDescription(
-                "You find yourself in a tavern like many others. Lively, smokey, reeking of ale " +
-                        "and men. A foul stench that sticks in your nose long after you have " +
-                        "left. You sip on your ale and wonder if another drunken evening awaits you. \n" +
-                        "The Tavern is made of a motley crew of bottom dwellers. A company of " +
-                        "whom" +
-                        " you’ve grown accustom to. Years of bad luck and failed adventures have " +
-                        "lead you to this point and you are not sure if it is a good thing or not.  \n"
-        );
 
     }
 }
